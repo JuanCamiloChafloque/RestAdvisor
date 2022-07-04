7ba99e6d8e31d277d85b6af8cd7b2c00e51cf22d
Camilo <camilochafloque@gmail.com>
2022-07-04 12:09:39 -0500

    Init firebase config

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { AppNavigation } from "./src/navigation/AppNavigation";
+import { initFirebase } from "./src/utils/firebase";
+
 export default function App() {
   return (
     <>
---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKDvJdGBA9Hc6khagZvdw-oiEI7UsV-ZI",
+  authDomain: "restadvisor-1153e.firebaseapp.com",
+  projectId: "restadvisor-1153e",
+  storageBucket: "restadvisor-1153e.appspot.com",
+  messagingSenderId: "548826320434",
+  appId: "1:548826320434:web:bf54e633299e0de782762b",
+};
+
+export const initFirebase = initializeApp(firebaseConfig);
