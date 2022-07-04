b055562579c5445c58dcca85b11c710941fd9169
Camilo <camilochafloque@gmail.com>
2022-07-04 10:41:03 -0500

    Installed all packages for navigation and UI

---
+++ src/screens/FavoritesScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export default class FavoritesScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Favorites Screen </Text>
+      </View>
+    );
+  }
+}
