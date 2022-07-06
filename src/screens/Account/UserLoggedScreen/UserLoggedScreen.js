2323993965811178ddcb4528465dd2f3c9f399b3
Camilo <camilochafloque@gmail.com>
2022-07-06 12:06:05 -0500

    Added logout functionality

--- src/screens/Account/UserLoggedScreen/UserLoggedScreen.js
+++ src/screens/Account/UserLoggedScreen/UserLoggedScreen.js
@@ -1,12 +1,25 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View } from "react-native";
+import { Button } from "react-native-elements";
+import { getAuth, signOut } from "firebase/auth";
 import InfoUser from "../../../components/account/InfoUser/InfoUser";
 import { styles } from "./UserLoggedScreenStyles";
 
 export default function UserLoggedScreen() {
+  const logout = async () => {
+    const auth = getAuth();
+    await signOut(auth);
+  };
+
   return (
     <View>
       <InfoUser style={styles.content} />
+      <Button
+        title="Cerrar Sesión"
+        buttonStyle={styles.btnStyles}
+        titleStyle={styles.btnText}
+        onPress={logout}
+      />
     </View>
   );
 }
--- src/screens/Account/UserLoggedScreen/UserLoggedScreenStyles.js
+++ src/screens/Account/UserLoggedScreen/UserLoggedScreenStyles.js
@@ -4,4 +4,19 @@ export const styles = StyleSheet.create({
   content: {
     minHeight: "100%",
   },
+
+  btnStyles: {
+    marginTop: 30,
+    borderRadius: 0,
+    paddingVertical: 10,
+    backgroundColor: "#fff",
+    borderTopWidth: 1,
+    borderTopColor: "#e3e3e3",
+    borderBottomWidth: 1,
+    borderBottomColor: "#e3e3e3",
+  },
+
+  btnText: {
+    color: "#00a680",
+  },
 });
