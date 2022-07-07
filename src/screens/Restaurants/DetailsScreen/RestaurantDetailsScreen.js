40340ffe1818fa78f2b59e7f8848be7fe9654b5b
Camilo <camilochafloque@gmail.com>
2022-07-07 18:36:56 -0500

    Obtaining restaurant data from firestore

--- src/screens/Restaurants/DetailsScreen/RestaurantDetailsScreen.js
+++ src/screens/Restaurants/DetailsScreen/RestaurantDetailsScreen.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import {
+  doc,
+  onSnapshot,
+  collection,
+  query,
+  where,
+  orderBy,
+} from "firebase/firestore";
+import { db } from "../../../utils/firebase";
 import { styles } from "./RestaurantDetailsScreenStyles";
 
 export default function RestaurantDetailsScreen(props) {
   const { route } = props;
+  const [restaurant, setRestaurant] = useState(null);
+
+  useEffect(() => {
+    setRestaurant(null);
+    onSnapshot(doc(db, "restaurants", route.params.id), (doc) => {
+      setRestaurant(doc.data());
+    });
+  }, [route.params.id]);
+
   console.log(route.params);
   return (
     <View>
